ea96e6f9e4be622e3120afb6c6f85e5668fe0d98
RaZz <amirrahmati471@gmail.com>
2025-04-22 20:33:51 +0330

    Implement ProtectedRoute for authentication and authorization; update routes in App component

---
+++ src/features/authentication/useAuthorized.ts
@@ -0,0 +1,37 @@
+import { useLocation } from "react-router";
+import useUser from "./useUser";
+
+export default function useAuthorized() {
+  const { user, isLoading } = useUser();
+  const { pathname } = useLocation();
+
+  let isAuthenticated = false;
+  if (user) isAuthenticated = true;
+
+  let isAuthorized = false;
+  // if (pathname.includes("owner")) {
+  //   if (user && user.role === "OWNER") isAuthorized = true;
+  // }
+
+  // if (pathname.includes("freelancer")) {
+  //   if (user && user.role === "FREELANCER") isAuthorized = true;
+  // }
+
+  // if (pathname.includes("admin")) {
+  //   if (user && user.role === "ADMIN") isAuthorized = true;
+  // }
+
+  const ROLES = {
+    admin: "ADMIN",
+    owner: "OWNER",
+    freelancer: "FREELANCER",
+  };
+
+  const desiredRole = pathname.split("/").at(1) as keyof typeof ROLES;
+
+  if (Object.keys(ROLES).includes(desiredRole)) {
+    if (user && user.role === ROLES[desiredRole]) isAuthorized = true;
+  }
+
+  return { isAuthenticated, isAuthorized, isLoading, user };
+}
